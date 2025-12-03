193c9fdbfed2943a20452712fbe407cf6712712c
ukkomiet <ukkomiet@gmail.com>
2025-12-03 23:12:34 +0200

    2

--- 2/2.cpp
+++ 2/2.cpp
@@ -12,20 +12,7 @@ int main() {
     for (string buff; (getline(idfile, buff, ','));) {
         id_ranges.push_back(buff);
     }
-    cout << id_ranges.size() << "\n";
 
-    /*
-    string temp = id_ranges[3];
-    int eka = stoi(temp.substr(0,temp.find('-')));
-    int toka = stoi(temp.substr(temp.find('-')+1));
-    cout << eka << " " << toka << "\n";
-    string eka_s = to_string(eka);
-    int s = eka_s.size()/2;
-    string a = eka_s.substr(0,s);
-    string b = eka_s.substr(s);
-    cout << "a:" + a << " " << "b: " + b << "\n";
-    */
-    
     long long sum = 0;
 
     // brutus
@@ -36,18 +23,26 @@ int main() {
         long long toka = stoll(t.substr(t.find('-')+1));
         for (eka; eka <= toka; eka++) {
             string s = to_string(eka);
-            string s0 = "";
             bool passes = true;
-            s0.push_back(s[0]);
-            int len = 1;
-            for (int j=1; j< s.size(); j++) {
-                if (s0 != s.substr(len,len)) {
-                    s0 = s.substr(0,j);
-                    passes = false;
-                    len = s0.size();
+            // check for same int
+            if (s.find_first_not_of(s[0]) == string::npos) {
+                // all same and passes
+            } else { // process substrings
+                string s0 = "";
+                s0.push_back(s[0]);
+                int len = 1;
+                int j = 1;
+                while (j*len < s.size() && s0 != s) {
+                    if (s0 != s.substr(j*len,len)) {
+                        s0 = s.substr(0,len+1);
+                        passes = false;
+                        len = s0.size();
+                        j = 1;
+                    } else {j++; passes = true;}
                 }
             }
-            if (passes) {
+            if (passes && s.size() > 1) {
+                //cout << s << "\n";
                 sum += eka;
             }
         }
