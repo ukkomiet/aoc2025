7ea5c4eded1500732fdc7f8f0e30b347fc7b0758
ukkomiet <ukkomiet@gmail.com>
2025-12-05 00:01:44 +0200

    3.1 kalloemoji

--- 3/3.cpp
+++ 3/3.cpp
@@ -17,36 +17,35 @@ int main() {
 
     long sum = 0;
 
-    int len = banks[0].size();
 
     #pragma omp parallel for
     for (int i = 0; i < banks.size(); i++) {
+
+        int len = banks[i].size();
         //cout << i << "\n";
         string s = banks[i];
         int first = 0;
         int second = 0;        
         // check values starting from 9
-        for (int ci = 9; ci > 0; ci--) {
-            if (first != 0 && second != 0) {break;}
+        int first_p = 0;
+        for (int c = 9; c > 0; c--) {
+            if (second != 0 && first != 0) {break;}
             
-            //cout << c << "\n";
+            size_t t = s.find_first_of(to_string(c),first_p+1);
+            while (t != string::npos) {
 
-            while (s.find_first_of(to_string(ci)) != string::npos) {
-                int pos = s.find_first_of(to_string(ci));
-                if (pos+1 != len && first == 0) {
-                    first = ci;
-                    s.erase(pos,1);
-                } else if (pos+1 == len && second == 0) {
-                    second = ci;
-                    s.erase(pos,1);
-                } else if (first != 0) {
-                    second = ci;
-                    s.erase(pos,1);
+
+                if (t+1!=len && first == 0) {
+                    first = c;
+                    first_p = t;
                 } else {
-                    break;
+                    second = c;
                 }
+                t = s.find_first_of(to_string(c),t+1);
             }
+            
         }
+        cout << s << " - " << 10*first + second << "\n";
         sum += second;
         sum += 10*first;
     }
