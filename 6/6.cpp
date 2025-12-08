5b837f14e0edbbca5f17e5cbbda7799dfd072446
ukkomiet <ukkomiet@gmail.com>
2025-12-08 15:54:53 +0200

    6.2 done

--- 6/6.cpp
+++ 6/6.cpp
@@ -14,104 +14,79 @@ void reverseString(string& str) {
     }
 }
 
+string filter_to_digits(string& str) {
+    string out = "";
+    for (char& c : str) {
+	    if (isdigit(c)) out += c;
+    }
+    return out;
+}
+
 int main() {
 
     vector<string> lines;
     ifstream infile("input.txt");
     for (string buff; (getline(infile, buff));) {
+        reverseString(buff);
         lines.push_back(buff);
     }
 
+    int input_length = lines[0].size();
+
     int operator_line = lines.size() - 1;
-
-    vector<vector<string>> list_of_lines(lines.size());
-    
-    // modify the string input lines into vectors, and place them in the list_of_lines vector
-    #pragma omp parallel for
-    for (int i = 0; i < lines.size(); i++) {
-        string s = lines[i];
-        reverseString(s); // reverse the string, since we read from right to left
-        istringstream stream(s);
-        string buff;
-        while (getline(stream, buff, ' ')) {
-            if (i == operator_line) {
-                if (buff != "" && buff != " ") { // disregard spaces in the operator line
-                    list_of_lines[i].push_back(buff);
-                }
-            } else {
-                if (buff == "" || buff == " ") { // problem is how to differentiate between (in input.txt) the last 9 columns, some start with spaces while some end with them...
-                    list_of_lines[i].back().push_back(' ');
-                } else {
-                    list_of_lines[i].push_back(buff);
-                }
-            }
-        }
-    }
-
-    int columns = list_of_lines[0].size();
-    int operators = list_of_lines[operator_line].size();
-    cout << "columns:" << columns << "\n";
-    cout << "nof ops:" << operators << "\n";
-
-    cout << "values (8-10) from row 0:" << list_of_lines[0][8] << "," << list_of_lines[0][9] << "," << list_of_lines[0][10] << "X" << "\n";
-    cout << "values (8-10) from row 1:" << list_of_lines[1][8] << "," << list_of_lines[1][9] << "," << list_of_lines[1][10] << "X" << "\n";
-    cout << "last 4 ops:" << list_of_lines[operator_line][operators-1][0] << list_of_lines[operator_line][operators-2][0] << list_of_lines[operator_line][operators-3][0] << list_of_lines[operator_line][operators-4][0] <<"\n";
-
     long long sum = 0;
     long long temp = 0;
 
-
-    #pragma omp parallel for
-    for (int col = 0; col < columns; col++) {
-        char op = list_of_lines[operator_line][col][0];
-
-        vector<string> numbers_str_temp;
-        int numbers_size = list_of_lines[0][col].size(); // how long each number is digits-wise
-
-        for (int i = 0; i < operator_line; i++) {
-            //cout << list_of_lines[i][col] << "-"<< "\n";
-            numbers_str_temp.push_back(list_of_lines[i][col]);
-        }
-
-        vector<long long> numbers;
-        
-        for (int spot = 0; spot < numbers_size; spot++) {
-            string temp_str = "";
-            for (string& n_str : numbers_str_temp) { // for every string, check every spot
-                if (isdigit(n_str[spot])) {
-                    temp_str.push_back(n_str[spot]);
-                }
-            }
-            cout << "---" << stoll(temp_str) << "---" << "\n"; 
-            long long temp_n = stoll(temp_str);
-            numbers.push_back(temp_n);
-        }
-
-        cout << "nubmer size:" << numbers.size() << "first:" << numbers[0] << "\n";
-
-
-        switch (op) {
-            case '+':
-                temp = 0;
-                for (long long& n : numbers) {
-                    cout << "n:" << n << "\n";
-                    temp += n;
-                }
-                sum += temp;
-                cout << "temp:" << temp << "\n";
-                break;
-            case '*':
-                temp = 1;
-                for (long long& n : numbers) {
-                    temp *= n;
-                }
-                sum += temp;
-                break;
-            default:
-                cout << "what the helly" << "\n";
-                break;
-        }
+    string empty_col = "";
+    for (int i = 0; i < lines.size(); i++) {
+        empty_col += " ";
     }
+
+    int spot = 0;
+    char op = 't';
+    vector<string> temp_strings;
+    string temp_str = "";
+    while (spot < input_length) { // iterate the input lines one character(spot) at a time
+        for (int row = 0; row < lines.size(); row++) {
+            temp_str += lines[row][spot];
+        }
+        if (temp_str != empty_col) {
+            temp_strings.push_back(temp_str);
+        }
+
+        if (temp_str.back() == '*' || temp_str.back() == '+') { // operator found which means this column of values is done and to be operated over
+            op = temp_str.back();
+
+            switch (op) {
+                case '+':
+                    temp = 0;
+                    for (string& str : temp_strings) {
+                        temp += stoll(filter_to_digits(str));
+                    }
+                    sum += temp;
+                    break;
+                case '*':
+                    temp = 1;
+                    for (string& str : temp_strings) {
+                        temp *= stoll(filter_to_digits(str));
+                    }
+                    sum += temp;
+                    break;
+                default:
+                    cout << "what the helly?" << "\n";
+                    break;
+            }
+
+            // clear the temp stuff
+            temp_strings.clear();
+            op = 't';
+        }
+        temp_str = "";
+        spot++;
+    }
+    
+    
+
 
     
     cout << sum << "\n";
